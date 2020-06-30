0bf642899e0a33db523cb6fdb4e90f69ca52abfc
Vegz78 <49032025+Vegz78@users.noreply.github.com>
2020-06-30 20:48:45 +0200

    Update _WakeGameStreamServer.sh

--- _WakeGameStreamServer.sh
+++ _WakeGameStreamServer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 clear
 echo "Waking Game Streaming Server..."
-sudo wakeonlan aa:bb:cc:dd:ee:ff
+sudo wakeonlan aa:bb:cc:dd:ee:ff  
+#You must install wakeonlan(sudo apt install wakeonlan) and change this MAC address to the MAC of your game streaming server
+#for this script to work
